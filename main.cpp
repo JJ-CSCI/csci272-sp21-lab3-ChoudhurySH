8b544aa57e2fd9ed862dd35587576366b2cce734
Shaad Choudhury <choudhuryshaad@gmail.com>
2021-03-02 18:59:17 +0000

    Final Push

--- main.cpp
+++ main.cpp
@@ -7,6 +7,61 @@
 // Put the assignment code here
 
 class Quaternion {
+public:
+  double w;
+  double x;
+  double y;
+  double z;
+
+  Quaternion (double a, double b, double c, double d){
+    w =a;
+    x = b;
+    y = c;
+    z = d;
+  }
+ 
+  
+  bool operator == (const Quaternion p) const{
+    if((w == p.w)&&(x == p.x)&&(y == p.y)&&(z ==p.z)){
+      return true;
+    }
+    else
+      return false;
+  }
+
+Quaternion operator+(const Quaternion q){
+  Quaternion r{
+    r.w = q.w + w,
+    r.x = q.x + x,
+    r.y = q.y + y,
+    r.z = q.z + z,
+  };
+  return r;
+  
+}
+Quaternion operator-(Quaternion q){
+  Quaternion r{
+    r.w = q.w - w,
+    r.x = q.x - x,
+    r.y = q.y - y,
+    r.z = q.z - z,
+  };
+  return r;
+}
+Quaternion operator*(double value){
+ Quaternion r{
+   r.w = w * value,
+   r.x = x * value,
+   r.y = y * value,
+   r.z = z * value,
+
+  };
+  return r;
+  
+}
+
+
+
 };
 
 //------------------------------
